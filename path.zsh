0f6e04291f1e54419b02ec643e795eca95a86386
Adam Loftin <aloftin@daxko.com>
2019-06-13 22:16:21 -0500

    yarn and nvm pathing

--- path.zsh
+++ path.zsh
@@ -2,7 +2,14 @@
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # Load Node global installed binaries
-export PATH="$HOME/.node/bin:$PATH"
+# export PATH="$HOME/.node/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+
+# Yarn packages
+PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
 
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
