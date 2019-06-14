5515a436bb6f74dc2402979ead588aaecad97131
Adam Loftin <aloftin@daxko.com>
2019-06-13 22:26:11 -0500

    alias tweaking

--- aliases.zsh
+++ aliases.zsh
@@ -4,7 +4,6 @@ alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
 weather() { curl -4 wttr.in/${1:-antwerp} }
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
 alias zbundle="antibody bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugins.sh"
@@ -12,14 +11,7 @@ alias zbundle="antibody bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugi
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias sites="cd $HOME/Sites"
-
-# Laravel
-alias a="php artisan"
-alias ams="php artisan migrate:fresh --seed"
-
-# PHP
-alias cfresh="rm -rf vendor/ composer.lock && composer i"
+alias daxko="cd $HOME/projects/daxko"
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
@@ -44,9 +36,9 @@ alias docker-composer="docker-compose"
 alias commit="git add . && git commit -m"
 alias gcommit="git add . && git commit"
 alias wip="commit wip"
-alias gst="git status"
+alias gs="git status"
 alias gb="git branch"
-alias gc="git checkout"
+alias gco="git checkout"
 alias gd="git diff"
 alias gl="git log --oneline --decorate --color"
 alias gnuke="git clean -df && git reset --hard"
