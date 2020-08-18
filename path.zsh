77c0b780da4b9f9462890745349a5aa5bbe19625
Adam Loftin <loftin.adam@gmail.com>
2020-08-18 10:41:56 -0500

    environment variable for netrc values for docker

--- path.zsh
+++ path.zsh
@@ -21,6 +21,9 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 # Postgres app 
 export PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
 
+#Docker NETRC
+export DOCKER_NETRC="$(cat ~/.netrc)"
+
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
