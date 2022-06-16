5af5a3e36cca83d3e31cbc54d0fbada24c2dd4ce
Adam Loftin <aloftin@MacBookPro-RF9HG6C6WG.local>
2022-06-15 21:28:28 -0500

    updated path to coreutils installed by homebrew

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,7 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
+alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
 weather() { curl -4 wttr.in/${1:-antwerp} }
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
