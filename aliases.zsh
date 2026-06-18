c6e94afbe3aa1b104f8edbc0fa045b829190a469
Adam Loftin <adamloftin@hey.com>
2026-06-18 11:42:53 -0500

    Update aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,17 @@
 # Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
-alias reloadcli="source $HOME/.zshrc"
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+alias reloadshell="omz reload"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-antwerp} }
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
-alias zbundle="antidote bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugins.sh"
+alias timestamp="date +%s"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
-alias rx="cd $HOME/projects/rxbenefits"
+alias projects="cd $HOME/Code"
+alias flt="cd $HOME/Code/fleetio"
 
 # Command line navigation
 alias ..="cd .."
@@ -22,14 +22,7 @@ alias .....="cd ../../../../.."
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
-
-# Vagrant
-alias v="vagrant global-status"
-alias vup="vagrant up"
-alias vhalt="vagrant halt"
-alias vssh="vagrant ssh"
-alias vreload="vagrant reload"
-alias vrebuild="vagrant destroy --force && vagrant up"
+alias watch="npm run dev"
 
 # Docker
 alias docker-composer="docker-compose"
@@ -40,21 +33,21 @@ alias docker-composer="docker-compose"
 #dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
 
 # Git
+alias gs="git status"
+alias gb="git branch --sort=-committerdate"
+alias gco="git checkout"
+alias gl="git log --oneline --decorate --color"
+alias gpo="git push origin"
 alias commit="git add . && git commit -m"
-alias gcommit="git add . && git commit"
+alias diff="git diff"
+alias force="git push --force-with-lease"
 alias wip="commit wip"
 alias ga="git add"
-alias ga.="git add ."
-alias gs="git status"
-alias gb="git branch"
-alias gco="git checkout"
-alias gd="git diff"
-alias gl="git log --oneline --decorate --color"
+alias pop="git stash pop"
+alias pull="git pull"
+alias stash="git stash -u"
 alias gnuke="git clean -df && git reset --hard"
-alias gpo="git push origin"
 
 # Go
 alias gocov="go test ./... --coverpkg ./... --coverprofile=cover_all.out;grep -v mocks cover_all.out > cover.out;go tool cover -html=cover.out;rm cover*.out"
 alias gocovt="go_test ./... --coverpkg ./... --coverprofile=cover_all.out;grep -v mocks cover_all.out > cover.out;go tool cover -func=cover.out;rm cover*.out"
-
-alias swagger='docker run --rm -it -e GOPATH=$HOME/go:/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger'
